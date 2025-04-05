e5414e5332d92c4c3dcc0a747821b3c13dc6477e
Hossein Amirabdollahi <hosseinamirabdollahi8@gmail.com>
2025-04-05 13:09:29 +0330

    Initialization

---
+++ src/main/java/com/training/studentmanagementapp/StudentmanagementappApplication.java
@@ -0,0 +1,13 @@
+package com.training.studentmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementappApplication.class, args);
+	}
+
+}
